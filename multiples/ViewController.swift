4ddcc2381e78046f3123f8b00729ee669480285d
G <g@Gs-MacBook-Pro.local>
2016-07-03 14:19:02 +0200

    1.commit from terminal

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,17 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Properties
+    var addNumber = 0
+    var leftSide = 0
+    var rightSide = 0
+    var clicked = 0
+    
+    // Outlets
+    @IBOutlet weak var MULTIPLESlogo: UIImageView!
+    @IBOutlet weak var PLAYbtn: UIButton!
+    @IBOutlet weak var QuestionTxtF: UITextField!
+    
+    @IBOutlet weak var ADDbtn: UIButton!
+    @IBOutlet weak var AddToaddLbl: UILabel!
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        
+        if QuestionTxtF.text != nil && QuestionTxtF.text != "" {
+ 
+            MULTIPLESlogo.hidden = true
+            PLAYbtn.hidden = true
+            QuestionTxtF.hidden = true
+            
+            ADDbtn.hidden = false
+            AddToaddLbl.hidden = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            
+            addNumber = Int(QuestionTxtF.text!)!
+            
+        }
+    }
+    
+    @IBAction func onAddBtnPressed(sender: UIButton!) {
+        
+        rightSide = leftSide + addNumber
+        AddToaddLbl.text = "\(leftSide)" + " + " + "\(addNumber)" + " = " + "\(rightSide)"
+        leftSide += addNumber
+        clicked += 1
+        
+        if isAddOver() {
+            restartGame()
+        }
+        
+        
+    }
+    
+    func restartGame() {
+        
+        addNumber = 0
+        leftSide = 0
+        rightSide = 0
+        clicked = 0
+        
+        QuestionTxtF.text = ""
+        AddToaddLbl.text = "Press ADD to add!"
+        
+        MULTIPLESlogo.hidden = false
+        PLAYbtn.hidden = false
+        QuestionTxtF.hidden = false
+        
+        ADDbtn.hidden = true
+        AddToaddLbl.hidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func isAddOver() -> Bool {
+        if clicked >= 6 {
+            return true
+        } else {
+            return false
+        }
     }
-
-
 }
-
